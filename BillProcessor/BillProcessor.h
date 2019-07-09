16dc6e74a047f1111c2ce5a53559af4155810b21
michael <michael@polarbear.fish>
2019-07-09 01:33:32 -0400

    Version 1.0

---
+++ BillProcessor/BillProcessor.h
@@ -0,0 +1,19 @@
+//
+//  BillProcessor.h
+//  BillProcessor
+//
+//  Created by Michael Chung on 7/9/19.
+//  Copyright © 2019 Michael Chung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BillProcessor.
+FOUNDATION_EXPORT double BillProcessorVersionNumber;
+
+//! Project version string for BillProcessor.
+FOUNDATION_EXPORT const unsigned char BillProcessorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BillProcessor/PublicHeader.h>
+
+
